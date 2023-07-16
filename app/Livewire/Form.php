0ec41656dc67e4cc6018f3426642a5bb6a2f9a2c
Dan Harrin <git@danharrin.com>
2023-07-16 09:17:30 +0100

    Update Form.php

--- app/Livewire/Form.php
+++ app/Livewire/Form.php
@@ -21,14 +21,101 @@ class Form extends Component implements HasForms, HasActions
 
     public function mount()
     {
-        $this->form->fill();
+        $this->form->fill([
+            'static' => 'one',
+            'staticMultiple' => ['one', 'two'],
+            'preloaded' => 'one',
+            'preloadedMultiple' => ['one', 'two'],
+            'notpreloaded' => 'one',
+            'notpreloadedMultiple' => ['one', 'two'],
+        ]);
     }
 
     protected function getFormSchema(): array
     {
-        return [
-            Forms\Components\MarkdownEditor::make('editor')->live(debounce: '1s'),
+        $options = [
+            'one' => 'One',
+            'two' => 'Two',
+            'three' => 'Three',
+            'four' => 'Four',
+            'five' => 'Five',
+            'six' => 'Six',
+            'seven' => 'Seven',
+            'eight' => 'Eight',
+            'nine' => 'Nine',
+            'ten' => 'Ten',
         ];
+
+        return [
+            Forms\Components\Select::make('static')
+                ->searchable()
+                ->options($options),
+            Forms\Components\Select::make('staticMultiple')
+                ->options($options)
+                ->multiple(),
+            Forms\Components\Select::make('preloaded')
+                ->searchable()
+                ->options(function () use ($options) {
+                    return $options;
+                })
+                ->getSearchResultsUsing(function (string $query) use ($options) {
+                    return collect($options)->filter(function (string $label) use ($query) {
+                        return str_contains(strtolower($label), strtolower($query));
+                    })->toArray();
+                })
+                ->getOptionLabelUsing(function (string $value) use ($options) {
+                    return $options[$value];
+                }),
+            Forms\Components\Select::make('preloadedMultiple')
+                ->options(function () use ($options) {
+                    return $options;
+                })
+                ->getSearchResultsUsing(function (string $query) use ($options) {
+                    return collect($options)->filter(function (string $label) use ($query) {
+                        return str_contains(strtolower($label), strtolower($query));
+                    })->toArray();
+                })
+                ->getOptionLabelsUsing(function (array $values) use ($options) {
+                    return collect($values)->mapWithKeys(function (string $value) use ($options) {
+                        return [$value => $options[$value]];
+                    })->toArray();
+                })
+                ->multiple(),
+            Forms\Components\Select::make('notpreloaded')
+                ->searchable()
+                ->getSearchResultsUsing(function (string $query) use ($options) {
+                    return collect($options)->filter(function (string $label) use ($query) {
+                        return str_contains(strtolower($label), strtolower($query));
+                    })->toArray();
+                })
+                ->getOptionLabelUsing(function (string $value) use ($options) {
+                    return $options[$value];
+                }),
+            Forms\Components\Select::make('notpreloadedMultiple')
+                ->getSearchResultsUsing(function (string $query) use ($options) {
+                    return collect($options)->filter(function (string $label) use ($query) {
+                        return str_contains(strtolower($label), strtolower($query));
+                    })->toArray();
+                })
+                ->getOptionLabelsUsing(function (array $values) use ($options) {
+                    return collect($values)->mapWithKeys(function (string $value) use ($options) {
+                        return [$value => $options[$value]];
+                    })->toArray();
+                })
+                ->multiple(),
+        ];
+    }
+
+    public function newData()
+    {
+        $this->form->fill([
+            'static' => 'nine',
+            'staticMultiple' => ['seven', 'eight'],
+            'preloaded' => 'nine',
+            'preloadedMultiple' => ['seven', 'eight'],
+            'notpreloaded' => 'nine',
+            'notpreloadedMultiple' => ['seven', 'eight'],
+        ]);
     }
 
     public function submit()
